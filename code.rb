a0bb5c0f2ec66d31bd06a191d4b2bcf5e7ee3fed
Mohammed Amin <mohammeda6429@hstat.org>
2019-01-10 16:06:28 +0000

    finish requirements

--- code.rb
+++ code.rb
@@ -1,2 +1,38 @@
 class Car
-end 
+  def initialize(color)
+    @color = color
+    @speed = 0
+    @speed_arr = [@speed]
+  end
+
+  def color
+    @color
+  end
+
+  def speed
+    @speed
+  end
+
+  def drive(speed)
+    for i in 1..speed
+      @speed = @speed+10
+      @speed_arr.push(@speed)
+    end
+  end
+end
+
+bugatti = Car.new("black")
+honda = Car.new("white")
+gtr = Car.new("silver")
+
+puts bugatti.color
+puts honda.color
+puts gtr.color
+
+bugatti.drive(5)
+honda.drive(3)
+gtr.drive(4)
+
+puts bugatti.speed
+puts honda.speed
+puts gtr.speed
